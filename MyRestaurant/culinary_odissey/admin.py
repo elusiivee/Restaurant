b296d8ac72480decc3e6d8d1e48e0f6ceae7241b
elusiivee <andrii.1669@gmail.com>
2023-12-26 22:25:28 +0100

    added Dishes

--- MyRestaurant/culinary_odissey/admin.py
+++ MyRestaurant/culinary_odissey/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import DishCategory
+from .models import DishCategory, Dish
 # Register your models here.
 admin.site.register(DishCategory)
+from django.utils.safestring import mark_safe
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('id', 'category', 'name', 'price', 'is_visible', 'photo_src_tag')
+    list_editable = ('name', 'category', 'price', 'is_visible',)
+    search_fields = ('name',)
+    list_filter = ('category','price', 'is_visible',)
+
+    def photo_src_tag(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' width=50>")
+
+    photo_src_tag.short_description = 'Dish photo'
--- MyRestaurant/culinary_odissey/models.py
+++ MyRestaurant/culinary_odissey/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class DishCategory(models.Model):
@@ -8,7 +9,35 @@ class DishCategory(models.Model):
     is_visible = models.BooleanField(default=True)
 
     class Meta:
-        verbose_name_plural='Disch categories'
-        ordering = ('order', )
+        verbose_name_plural = 'Disch categories'
+        ordering = ('order',)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __iter__(self):
+        dishes = self.dishes.filter(is_visible=True)
+        for dish in dishes:
+            yield dish
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='url')
+    ingredients = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField(upload_to='dishes/', blank=True)
+    category = models.ForeignKey(DishCategory, on_delete=models.PROTECT, related_name='dishes')
+    is_visible = models.BooleanField(default=True)
+    order = models.PositiveSmallIntegerField()
+
+    class Meta:
+        verbose_name_plural = 'Dishes'
+        ordering = ('order',)
+        constraints = [
+            models.UniqueConstraint(fields=['order', 'category'], name='unique_order_per_each_category'),
+        ]
+        unique_together = ['id', 'slug']
+
     def __str__(self):
         return f'{self.name}'

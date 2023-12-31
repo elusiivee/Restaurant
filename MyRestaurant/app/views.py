1c202ddbfff81ad505b7cd0d402cb497944b800d
elusiivee <andrii.1669@gmail.com>
2023-12-31 16:12:08 +0100

    fixed issues in app/views

--- MyRestaurant/app/views.py
+++ MyRestaurant/app/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
-# from .models import MainMenueItems
-# def menu(request):
-#     category = MainMenueItems.objects.filter(is_visible=True)
-#     return render(request, 'includes/menu.html')
-#
+from django.shortcuts import render
+from .models import MainMenueItems
+def menu(request):
+    category = MainMenueItems.objects.filter(is_visible=True)
+    return render(request, 'includes/menu.html')
+

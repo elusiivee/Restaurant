9fd3e9d3741a21b47f6a1b2eef19744123bf3d9b
elusiivee <andrii.1669@gmail.com>
2023-12-31 19:31:23 +0100

    finally created menu section

--- MyRestaurant/app/admin.py
+++ MyRestaurant/app/admin.py
@@ -6,5 +6,5 @@ from .models import MainMenueItems, Footer
 class MainMenuItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title','slug', 'is_visible', 'order')
-    list_editable = ('is_visible', 'order')
+    list_editable = ('is_visible', 'order',)
 admin.site.register(Footer)
--- MyRestaurant/culinary_odissey/views.py
+++ MyRestaurant/culinary_odissey/views.py
@@ -3,5 +3,10 @@ from .models import Dish, DishCategory
 # Create your views here.
 def main(request):
     category = DishCategory.objects.filter(is_visible=True)
-    return render(request, 'main_file.html')
+    dish = Dish.objects.filter(is_visible=True)
+    context = {
+        'categories':category,
+        'dishes' : dish,
+    }
+    return render(request, 'main_file.html', context=context)
 

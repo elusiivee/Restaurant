7aae0535adf67353ebaaf122ee94ee60ff7d268b
elusiivee <andrii.1669@gmail.com>
2024-01-07 21:26:15 +0100

    dixed input forms, added calendar

--- MyRestaurant/culinary_odissey/forms.py
+++ MyRestaurant/culinary_odissey/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import Reservation
-from django.core.validators import RegexValidator
-import re
+
 class ReservationForm(forms.ModelForm):
     name = forms.CharField(label='Name', widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Your Name'}
@@ -12,18 +11,12 @@ class ReservationForm(forms.ModelForm):
     phone = forms.CharField(label='Phone', widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Your phone'}
     ))
-    date = forms.DateField(label='Date', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'YYYY-MM-DD'}
-    ), validators=[RegexValidator(
-        regex=r'^\d{4}-\d{2}-\d{2}$',
-        message='Enter a valid date in the format YYYY-MM-DD.',
-    )])
-    time = forms.TimeField(label='Time', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'HH:MM'}
-    ), validators=[RegexValidator(
-        regex=r'^\d{2}:\d{2}$',
-        message='Enter a valid time in the format HH:MM.',
-    )])
+    date = forms.DateField(label='Date', widget=forms.DateInput(
+        attrs={'class': 'form-control datepicker', 'placeholder': 'YYYY-MM-DD'}
+    ), input_formats=['%Y-%m-%d'])
+    time = forms.TimeField(label='Time', widget=forms.TimeInput(
+        attrs={'class': 'form-control timepicker', 'placeholder': 'HH:MM'}
+    ), input_formats=['%I:%M%p', '%H:%M'])
     people = forms.IntegerField(label='People', widget=forms.NumberInput(
         attrs={'class': 'form-control', 'placeholder': '#People'}
     ))

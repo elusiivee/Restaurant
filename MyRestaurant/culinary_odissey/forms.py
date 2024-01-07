71f9825f9a44ad95c536984d8cec79777ee8dfb7
elusiivee <andrii.1669@gmail.com>
2024-01-07 15:14:52 +0100

    fixed problem with map

--- MyRestaurant/culinary_odissey/forms.py
+++ MyRestaurant/culinary_odissey/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Reservation
-
+from django.core.validators import RegexValidator
+import re
 class ReservationForm(forms.ModelForm):
     name = forms.CharField(label='Name', widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Your Name'}
@@ -13,10 +14,16 @@ class ReservationForm(forms.ModelForm):
     ))
     date = forms.DateField(label='Date', widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'YYYY-MM-DD'}
-    ))
+    ), validators=[RegexValidator(
+        regex=r'^\d{4}-\d{2}-\d{2}$',
+        message='Enter a valid date in the format YYYY-MM-DD.',
+    )])
     time = forms.TimeField(label='Time', widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'HH:MM'}
-    ))
+    ), validators=[RegexValidator(
+        regex=r'^\d{2}:\d{2}$',
+        message='Enter a valid time in the format HH:MM.',
+    )])
     people = forms.IntegerField(label='People', widget=forms.NumberInput(
         attrs={'class': 'form-control', 'placeholder': '#People'}
     ))

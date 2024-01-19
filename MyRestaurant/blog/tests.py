e50df21cc8a79645b590e94b0d7451c52b8bd654
elusiivee <andrii.1669@gmail.com>
2024-01-19 15:10:52 +0100

    added test to blog application

--- MyRestaurant/blog/tests.py
+++ MyRestaurant/blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Blog
+class BlogTest(TestCase):
+    '''
+    Class BlogTest testing model Blog
 
-# Create your tests here.
+    Methods:
+        setUp(self): Creates a sample blog item for testing.
+
+        test_blog_item_attributes(self): Tests the attributes of the menu item created in setUp.
+    '''
+
+    def setUp(self):
+        Blog.objects.create(title='Item1', content='Content1', is_visible=True)
+
+    def test_blog_item_attributes(self):
+        blog_item = Blog.objects.get(title="Item1")
+
+        self.assertEqual(blog_item.title, "Item1")
+        self.assertEqual(blog_item.content, "Content1")
+        self.assertTrue(blog_item.is_visible)
